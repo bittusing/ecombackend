d99b6b850a4deb9c70743f14e3f581aa135d8226
Abhilekh Singh <abhilekhk6@gmail.com>
2024-02-04 16:31:15 +0530

    bvc

--- models/subcategoryModel.js
+++ models/subcategoryModel.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
 
 const subcategorySchema = new mongoose.Schema(
-    {
-        subcategory: {
-          type: String,
-          trim: true,
-        } 
-      },
   {
+    subcategory: {
+      type: String,
+      trim: true,
+    },
     category: {
-        type: mongoose.Schema.ObjectId,
-        required:[true,"Please Enter Category"],
-         trim: true,
-    } 
+      type: mongoose.Schema.ObjectId,
+      required:[true,"Please Enter category Mobile"],     
+      trim: true,
+    },
   },
   {
     timestamps: true,
