54bb99dd48610cce60408bde5d8d2c6277d0e445
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2024-03-14 12:40:09 +0530

    cbv

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -1,33 +1,55 @@
-const Cart=require('../models/cartModel'); 
+const Cart = require('../models/cartModel');
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
+exports.addCart = catchAsyncErrors(async (req, res, next) => {
+  const { productid, session_id, productWeight, user_id } = req.body;
+  let cart = await Cart.findOne({ productid, session_id, productWeight });
+  if (cart) {
+    const updatedQuantity = parseInt(cart.Quantity) + 1;
+    const update_data = { Quantity: updatedQuantity };
+    cart = await Cart.findOneAndUpdate({ productid, session_id, productWeight }, update_data, { new: true });
+  } else {
+    cart = await Cart.create(req.body);
+  }
+  res.status(201).json({
+    success: true,
+    message: "Added to Cart List Successfully",
+    cart,
+  });
+});
 
-exports.addCart=catchAsyncErrors(async (req,res,next)=>{
-
-          const cart=await Cart.create(req.body);
-
-          res.status(201).json({
-           success: true,
-           message:"Added to Cart List Successfully",
-           cart,
-         });  
+exports.addCartDecreaseQuantity = catchAsyncErrors(async (req, res, next) => {
+  const { productid, session_id, productWeight, user_id } = req.body;
+  let cart = await Cart.findOne({ productid, session_id, productWeight });
+  if (cart) {
+    const updatedQuantity = parseInt(cart.Quantity) - 1;
+    const update_data = { Quantity: updatedQuantity };
+    cart = await Cart.findOneAndUpdate({ productid, session_id, productWeight }, update_data, { new: true });
+  } else {
+    cart = await Cart.create(req.body);
+  }
+  res.status(201).json({
+    success: true,
+    message: "Added to Cart List Successfully",
+    cart,
+  });
 });
 
 //////  delete all for work 
 
-exports.deleteaddcart=catchAsyncErrors(async (req,res,next)=>{
+exports.deleteaddcart = catchAsyncErrors(async (req, res, next) => {
 
-  const cart=await Cart.deleteMany();
+  const cart = await Cart.deleteMany();
 
   res.status(201).json({
-   success: true,
-   message:"Deleted All Successfully",
- });  
+    success: true,
+    message: "Deleted All Successfully",
+  });
 });
 
 /////////removecartbycartid
-exports.removecartbycartid=catchAsyncErrors(async(req,res,next)=>{
+exports.removecartbycartid = catchAsyncErrors(async (req, res, next) => {
   const cart = await Cart.findById(req.params.id);
 
   if (!cart) {
@@ -43,12 +65,12 @@ exports.removecartbycartid=catchAsyncErrors(async(req,res,next)=>{
 })
 
 /// get by session id 
-exports.getAllCartBySessionId=catchAsyncErrors(async (req,res,next)=>{
-           const {session_id} =req.body;
-            const cart=await Cart.find({session_id});
-           res.status(201).json({
-            success: true,
-            message:"Get All Successfully",
-            cart
-          });  
+exports.getAllCartBySessionId = catchAsyncErrors(async (req, res, next) => {
+  const { session_id } = req.body;
+  const cart = await Cart.find({ session_id });
+  res.status(201).json({
+    success: true,
+    message: "Get All Successfully",
+    cart
+  });
 });
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,12 +1,13 @@
 const express=require('express');
 
-const { addCart ,deleteaddcart,getAllCartBySessionId ,removecartbycartid} = require('../controllers/cartControllers');
+const { addCart ,deleteaddcart,getAllCartBySessionId ,removecartbycartid, addCartDecreaseQuantity} = require('../controllers/cartControllers');
 const {getAllwishlistByUserId,deleteaddwishlist,addWishlist ,removewishlistbycartid}=require('../controllers/wishlistController');
 
 const router=express.Router();
   
 router.route("/deleteaddcart").delete(deleteaddcart); 
 router.route("/addCart").post(addCart); 
+router.route("/addCartDecreaseQuantity").post(addCartDecreaseQuantity); 
 router.route("/removecartbycartid/:id").delete(removecartbycartid); 
 router.route("/getAllCartBySessionId").post(getAllCartBySessionId); 
 
