0c93bec6001c59af52ad8fc91901b0b1d54ebf45
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2024-03-22 15:27:30 +0530

    uplode

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -3,26 +3,26 @@ const AgentAddress = require("../models/agentAddressModel");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const sendToken = require("../utils/jwtToken");
-const LoginHistory=require("../models/LoginHistory");
+const LoginHistory = require("../models/LoginHistory");
 const bcrypt = require('bcryptjs');
 const useragent = require('express-useragent');
 
 exports.createAgent = catchAsyncErrors(async (req, res, next) => {
-const agent = await Agent.create(req.body);
-res.status(201).json({
-  success: true,   
-  agent,
-  message:"Agent Added Successfully...." 
-});  
+  const agent = await Agent.create(req.body);
+  res.status(201).json({
+    success: true,
+    agent,
+    message: "Agent Added Successfully...."
+  });
 });
 
 ///// Addresss Add 
-exports.AddAgentAddress=catchAsyncErrors(async(req,res,next)=>{
+exports.AddAgentAddress = catchAsyncErrors(async (req, res, next) => {
   const agentAddress = await AgentAddress.create(req.body);
-  res.status(201).json({  
-    success: true,   
-    agentAddress,  
-    message:"Address Added Successfully...." 
+  res.status(201).json({
+    success: true,
+    agentAddress,
+    message: "Address Added Successfully...."
   });
 })
 
@@ -38,6 +38,24 @@ exports.deleteAgentAddress = catchAsyncErrors(async (req, res, next) => {
     success: true,
     message: "Address Delete Successfully",
     agent,
+  });
+});
+
+//////////  EditAgentAddress
+exports.EditAgentAddress = catchAsyncErrors(async (req, res, next) => {
+  const agentAddress = await AgentAddress.findById(req.params.id);
+  if (!agentAddress) {
+    return next(new ErrorHander("Agent Address is not", 400));
+  }
+  const updateagent = await AgentAddress.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  })
+
+  res.status(200).json({
+    success: true,
+    updateagent,
   });
 });
 
@@ -61,39 +79,39 @@ exports.deleteAgent = catchAsyncErrors(async (req, res, next) => {
 // get all agent --admin
 
 exports.getAllAgent = catchAsyncErrors(async (req, res, next) => {
-  
+
   // const agent = await Agent.find({role:"user"});
   const agent = await Agent.find();
-  
+
 
   res.status(201).json({
     success: true,
-    agent, 
-    
+    agent,
+
   });
-});     
+});
 
 
 // get Agent  details
 
 exports.getAgentDetails = catchAsyncErrors(async (req, res, next) => {
-  
+
   const agent = await Agent.findById(req.params.id);
 
   if (!agent) {
-    return next(new ErrorHander("Agent Not Found", 404));    
+    return next(new ErrorHander("Agent Not Found", 404));
   }
-  
+
   res.status(201).json({
     success: true,
     agent,
-  }); 
-});  
+  });
+});
 
 // login Agent
 
 exports.loginAgent = catchAsyncErrors(async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
   if (!email || !password) {
     return next(new ErrorHander("Plz Enter Email And Password", 400));
@@ -109,102 +127,102 @@ exports.loginAgent = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHander("Invalid email Or password", 400));
   }
   const userAgent = req.useragent;
-  
- 
+
+
   const token = agent.getJWTToken();
- 
+
   sendToken(agent, 200, res);
-  
-  
+
+
 });
 /// update Client Access
-exports.updateClientAccess=catchAsyncErrors(async(req,res,next)=>{
-    ///const  {client_access}=req.body;
+exports.updateClientAccess = catchAsyncErrors(async (req, res, next) => {
+  ///const  {client_access}=req.body;
   const agent = await Agent.findById(req.params.id);
-  if(!agent){   
+  if (!agent) {
     return next(new ErrorHander("Invalid email Or password", 400));
   }
-   
-   
-  const agent_access=await agent.client_access;
- 
-  if(agent_access==='yes'){
-   const agent=await Agent.updateOne({_id:req.params.id},{$set: {client_access:"no"}});
+
+
+  const agent_access = await agent.client_access;
+
+  if (agent_access === 'yes') {
+    const agent = await Agent.updateOne({ _id: req.params.id }, { $set: { client_access: "no" } });
   }
-  if(agent_access==='no'){
-    const agent=await Agent.updateOne({_id:req.params.id},{$set: {client_access: "yes"}});
-    
+  if (agent_access === 'no') {
+    const agent = await Agent.updateOne({ _id: req.params.id }, { $set: { client_access: "yes" } });
+
   }
   res.status(201).json({
     success: true,
-    agent, 
-    
+    agent,
+
   });
- });
+});
 
 
- exports.EditAgentDetails=catchAsyncErrors(async(req,res,next)=>{
+exports.EditAgentDetails = catchAsyncErrors(async (req, res, next) => {
   const agent = await Agent.findById(req.params.id).select(
     "+agent_password"
   );
-  if(!agent){   
+  if (!agent) {
     return next(new ErrorHander("Invalid email Or password", 400));
   }
- if(!req.body.agent_password){ 
-     const  updateagent=await Agent.findByIdAndUpdate(req.params.id,req.body,{   
-    new:true,    
-    runValidators:true,    
-    useFindAndModify:false,
-})
+  if (!req.body.agent_password) {
+    const updateagent = await Agent.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    })
 
-res.status(200).json({     
-  success: true, 
-  updateagent,  
-});
- }else{
+    res.status(200).json({
+      success: true,
+      updateagent,
+    });
+  } else {
 
-  const isPasswordMatched = await agent.comparePassword(req.body.agent_password);
-  if (!isPasswordMatched) { 
-     
-   const convertohashpass=  await bcrypt.hash(req.body.agent_password,10);
-     const {agent_password ,...newAaa}=await req.body;
-     const updatekrnewaladata= await {...newAaa,agent_password:convertohashpass};
-     const  updateagent=await Agent.findByIdAndUpdate(req.params.id,updatekrnewaladata,{   
-      new:true,    
-      runValidators:true,    
-      useFindAndModify:false,
-  })
-  
-  res.status(200).json({     
-    success: true, 
-    updateagent,  
-  });
-  }else{
-    const  updateagent=await Agent.findByIdAndUpdate(req.params.id,req.body,{   
-      new:true,    
-      runValidators:true,    
-      useFindAndModify:false,
-  })
-  
-  res.status(200).json({     
-    success: true, 
-    updateagent,  
-  });
+    const isPasswordMatched = await agent.comparePassword(req.body.agent_password);
+    if (!isPasswordMatched) {
+
+      const convertohashpass = await bcrypt.hash(req.body.agent_password, 10);
+      const { agent_password, ...newAaa } = await req.body;
+      const updatekrnewaladata = await { ...newAaa, agent_password: convertohashpass };
+      const updateagent = await Agent.findByIdAndUpdate(req.params.id, updatekrnewaladata, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+      })
+
+      res.status(200).json({
+        success: true,
+        updateagent,
+      });
+    } else {
+      const updateagent = await Agent.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+      })
+
+      res.status(200).json({
+        success: true,
+        updateagent,
+      });
+    }
+
   }
 
- }
 
 
 
-
- })
-
+})
 
 
- exports.forgotPassword=catchAsyncErrors(async (req,res,next)=>{
-       //    const {email,new_password}=req.body;
 
- });
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  //    const {email,new_password}=req.body;
+
+});
 
 
 
