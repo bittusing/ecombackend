5b87d3225d3b5da464b7241569aa21af04f27187
bittusing <abhilekhk6@gmail.com>
2024-05-18 11:45:46 +0530

    update

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,6 +1,7 @@
 const Wishlist = require('../models/wishListModel');
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Product =require('../models/productModel');
 
 
 exports.addWishlist = catchAsyncErrors(async (req, res, next) => {
@@ -50,12 +51,26 @@ exports.deleteaddwishlist = catchAsyncErrors(async (req, res, next) => {
 });
 
 /// get by session id 
+/// get by session id 
 exports.getAllwishlistByUserId = catchAsyncErrors(async (req, res, next) => {
     const { user_id } = req.body;
     const wishlist = await Wishlist.find({ user_id });
+    const wishlistArray = [];
+
+    for (const wish of wishlist) {
+        const product_id = wish?.productid;
+        const product = await Product.findById(product_id).select('Stock');
+        const stock = product ? product.Stock : null;
+
+        wishlistArray.push({
+            ...wish._doc, // Spread the existing wishlist document
+            Stock: stock, // Add the Stock field
+        });
+    }
+
     res.status(201).json({
         success: true,
         message: "Get All Successfully",
-        wishlist
+        wishlist: wishlistArray,
     });
 });
